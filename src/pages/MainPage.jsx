1178118524020148720a5bf01806f0e02af4e901
sayyyho <323psh@naver.com>
2025-07-30 01:49:16 +0900

    chore

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,9 @@
+import { useCountdown } from "../hooks/useCountdown";
 import { useWindowSize } from "../hooks/useWindowSize";
 
 export const MainPage = () => {
   const { width, height } = useWindowSize();
+  const { days, hours, minutes, seconds } = useCountdown();
   return (
     <div>
       <h1>width: {width}</h1>
