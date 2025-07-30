6faa2812c71054adffb9ad794cc5f4fa633a963e
seoyeon435 <0305psy@naver.com>
2025-07-30 20:03:18 +0900

    Feature: useAlert 구현

--- src/components/YourOwnHookPage.jsx
+++ src/components/YourOwnHookPage.jsx
@@ -1,11 +1,16 @@
-import { useSomething } from "../hooks/useSomething";
+import { useModal } from "../hooks/useAlert";
 
 export const YourOwnHookPage = () => {
+  const { modal } = useModal();
   // const { something... } = useSomething();
   // 하단 UI에 자유롭게 위에서 받아온 값들을 바인딩 해보세요~
   return (
     <div>
       <h2>useSomething 실습</h2>
+      <button className="modern-btn" onClick={modal}>
+        눌러바 궁금하지?? ... 그치?
+      </button>
     </div>
   );
 };
+
