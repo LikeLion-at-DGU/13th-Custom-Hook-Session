2f714bf7985ba634caab1080865a03f4b21945b5
MORUN <hyojunj@naver.com>
2025-07-30 19:21:47 +0900

    Refactor: useWindowSize로 리펙토링

--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
+const targetDate = new Date("2025-08-25T00:00:00");
 
 export const useCountdown = (targetDate) => {
+  const difference = targetDate.getTime() - Date().getTime();
+
+  const Days = Math.floor(difference / (1000*60*60*24));
 
 
 
