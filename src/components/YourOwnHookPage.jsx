f8a200a971faa4e654e4d84c563d2c82818d85c4
이민지 <minji@iminjiui-MacBookAir.local>
2025-07-30 20:05:23 +0900

    Feature: useFireworks 구현

--- src/components/YourOwnHookPage.jsx
+++ src/components/YourOwnHookPage.jsx
@@ -1,11 +1,26 @@
-import { useSomething } from "../hooks/useSomething";
+import { useFireworks } from "../hooks/useFireworks";
 
 export const YourOwnHookPage = () => {
-  // const { something... } = useSomething();
-  // 하단 UI에 자유롭게 위에서 받아온 값들을 바인딩 해보세요~
+  const { fire, containerRef } = useFireworks();
+
   return (
     <div>
-      <h2>useSomething 실습</h2>
+      <h2>useFireworks 예제</h2>
+
+      {/* 불꽃놀이가 표시될 영역 */}
+      <div
+        ref={containerRef}
+        style={{
+          width: "100%",
+          height: "300px",
+          background: "#000",
+          marginBottom: "20px",
+        }}
+      />
+
+      <button className="modern-btn" onClick={fire}>
+        불꽃놀이 발사
+      </button>
     </div>
   );
 };
