a37c87bcc3edea1f045edc511ffcd4ce2651e3a3
jyeon03 <bjlyeon2003@naver.com>
2025-07-30 19:19:27 +0900

    Refactor: useCountdown, calculateTime으로 리팩토링

--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import { calculateTimeLeft } from "../utils/calculateTime";
+
 export const useCountdown = (targetDate) => {
+  const [timeLeft, setTimeLeft] = useState(() => calculateTimeLeft(targetDate));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft(targetDate));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
   return timeLeft;
 };
