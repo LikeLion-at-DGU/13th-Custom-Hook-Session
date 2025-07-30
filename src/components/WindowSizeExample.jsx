e8212a97b9908a1b744618c31ec32a9168b813d7
Hyunuk-Lee <air244302@gmail.com>
2025-07-30 19:40:11 +0900

    Refactor: useWindowSize로 리팩토링 수정

--- src/components/WindowSizeExample.jsx
+++ src/components/WindowSizeExample.jsx
@@ -1,12 +1,15 @@
-import { useWindowSize } from "../hooks/useWindowSize"
+import { useEffect, useState } from "react";
+import { useWindowSize } from '../hooks/useWindowSize';
 
-export const MainPage = () => {
-  const { useWindowSize } = useWindowSize();
-  return (
-    <div>
-      <h2>useWindowSize 실습</h2>
-      <p>화면 너비: {windowSize.width}px</p>
-      <p>화면 높이: {windowSize.height}px</p>
-    </div>
-  );
-};
+export const WindowSizeExample = () => {
+
+    const { windowSize } = useWindowSize();
+
+    return (
+        <div>
+        <h2>useWindowSize 실습</h2>
+        <p>화면 너비: {windowSize.width}px</p>
+        <p>화면 높이: {windowSize.height}px</p>
+        </div>
+    );
+}
