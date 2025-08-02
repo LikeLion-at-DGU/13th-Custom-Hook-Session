716dec48555d5a85cee1822dad9502f647bc8881
Soi-coding <2023112594@dgu.ac.kr>
2025-08-02 09:58:58 +0900

    Refactor: useWindowSize로 리팩토링

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,1 +1,21 @@
-// 커스텀훅 코드를 작성해보세요!
+import { useState, useEffect } from "react";
+
+export default function useWindowSize() {
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return size;
+}
